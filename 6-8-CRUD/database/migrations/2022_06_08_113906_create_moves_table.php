216245c329535440fa4319a13f539113c101a17a
shifaalkasasbeh <100359607+shifaalkasasbeh@users.noreply.github.com>
2022-06-09 15:08:37 +0300

    main

---
+++ 6-8-CRUD/database/migrations/2022_06_08_113906_create_moves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->id();
+            $table->string('movie_name');
+            $table->string('movie_description');
+            $table->string('movie_gener');
+            $table->string('image_name')->nullable();
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+}
